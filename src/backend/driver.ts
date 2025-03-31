920d3dbc095f800879acac8e6b4bb018ee600b98
dan <dan.holmberg1@hotmail.com>
2025-03-31 16:35:32 +0200

    Most of backend...

---
+++ src/backend/driver.ts
@@ -0,0 +1,34 @@
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import './db_opertions.ts'
+import { DBInit, routeGet } from './db_opertions.ts';
+import { Database } from 'sqlite';
+
+const httpServer = createServer();
+const io = new Server(httpServer, { cors: { origin: '*' } });
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+
+  // Sending data to client
+  socket.emit('welcome', { message: 'Hello from the backend via WebSocket!' });
+
+  // Receiving data from client
+  socket.on('clientData', (data) => {
+    console.log('Data from client:', data);
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log('WebSocket server running on port 3000');
+});
+
+
+async function main() {
+  let db: Database = await DBInit();
+  const res = await routeGet(db, 100);
+  console.log(res);
+}
+
+
+main()
