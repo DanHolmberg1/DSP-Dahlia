7eadd41f37bea0ebc337935293e84d87aaad87f5
Dan Holmberg <144323727+DanHolmberg1@users.noreply.github.com>
2025-03-31 15:41:56 +0200

    Update StartEndTripRoutingAPI.tsx

--- src/frontend/my-app/components/StartEndTripRoutingAPI.tsx
+++ src/frontend/my-app/components/StartEndTripRoutingAPI.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import MapView, { Polyline, Marker } from "react-native-maps";
 
-const ORS_API_KEY = '5b3ce3597851110001cf6248c932f24e6e9e4ac58186a327506210a4';
 
 
 export const getStartEndTrip = async(start: {latitude: number, longitude:number} | null, end: {latitude: number, longitude:number} | null, seed: number, p: number) => {
