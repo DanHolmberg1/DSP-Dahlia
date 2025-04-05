93e0515280b4a1fe0f606d32911a118f1a760408
dan <dan.holmberg1@hotmail.com>
2025-04-05 17:44:03 +0200

    something

--- src/frontend/my-app/components/RoundTripRoutingAPI.tsx
+++ src/frontend/my-app/components/RoundTripRoutingAPI.tsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import MapView, { Polyline, Marker } from "react-native-maps";
 
-const ORS_API_KEY = '';
+import api_key from '../secrets.env'
+
+const ORS_API_KEY = api_key;
 /**
  * Function that makes an API call to OpenRouteService to get the desired route.
  * 
@@ -16,40 +18,40 @@ const ORS_API_KEY = '';
  * which is usually a series of coordinates (latitude and longitude). 
  * This data is typically used to render a route on a map.
  */
-export const getRoundTripRoute = async (start: {latitude: number, longitude:number} | null, len: number, seed: number, p: number) => {
-    if (start == null) {
-        console.error("Something went wrong with the coordinates.");
-        return;
-    }
-    try {
-        const response = await fetch("https://api.openrouteservice.org/v2/directions/foot-walking", {
-            method: "POST",
-            headers: {
-                Authorization: ORS_API_KEY,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                coordinates: [[start.longitude, start.latitude]], // Start/end coordinate
-                options: { 
-                    round_trip: { // Round-trip: start and end points are the same
-                        length: len,  // Length of the route (in meters)
-                        seed: seed,   // Random seed for the route calculation
-                        points: p // Number of points for the round trip
-                    }
-                }
-            }),  
-        });
-     
-        const data = await response.json();
-        if(data.routes && data.routes.length > 0 && data.routes[0].geometry) {
-            console.log("data:", data);
-            return data.routes[0];
-        }else {
-            console.error("route data is invalid")
+export const getRoundTripRoute = async (start: { latitude: number, longitude: number } | null, len: number, seed: number, p: number) => {
+  if (start == null) {
+    console.error("Something went wrong with the coordinates.");
+    return;
+  }
+  try {
+    const response = await fetch("https://api.openrouteservice.org/v2/directions/foot-walking", {
+      method: "POST",
+      headers: {
+        Authorization: ORS_API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        coordinates: [[start.longitude, start.latitude]], // Start/end coordinate
+        options: {
+          round_trip: { // Round-trip: start and end points are the same
+            length: len,  // Length of the route (in meters)
+            seed: seed,   // Random seed for the route calculation
+            points: p // Number of points for the round trip
+          }
         }
-    } catch (error) {
-        console.error("API error:", error);
+      }),
+    });
+
+    const data = await response.json();
+    if (data.routes && data.routes.length > 0 && data.routes[0].geometry) {
+      console.log("data:", data);
+      return data.routes[0];
+    } else {
+      console.error("route data is invalid")
     }
+  } catch (error) {
+    console.error("API error:", error);
+  }
 };
 
 
