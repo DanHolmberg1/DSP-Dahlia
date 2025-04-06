9e4f6f5293540bae80db1df2fbaa388946d9bac4
EllenAkerlind <ellenviola04@outlook.com>
2025-04-06 10:46:49 +0200

    A few updates

--- src/backend/webSocket.ts
+++ src/backend/webSocket.ts
@@ -1,10 +1,13 @@
 import { WebSocketServer } from "ws";
-import { DBInit, routeAdd, routeGet, clearGroups, clearRoutes,  clearUsers } from "./db_opertions";
-import { Database } from 'sqlite';
+import { DBInit, routeAdd, routeGet, clearGroups, clearRoutes,  clearUsers, groupAdd, groupCreate } from "./db_opertions";
+
+//Lägg till signal handler för graceful shutdown kanske? 
 
 interface typeOfData{
   type: string, 
-  data: JSON
+  data: JSON, 
+  id: number, 
+  message?: string
 }
 
 const port = 3000;
@@ -13,27 +16,54 @@ console.log("Server is running on port 3000");
 
 wss.on('connection', (ws) => {
   ws.send("Hello client");
+
   ws.on('message', async (recievedData) => {
     console.log(recievedData);
-    const data: typeOfData = JSON.parse(recievedData.toString());
-    console.log(data.type);
-    
+    try{
+      const data: typeOfData = JSON.parse(recievedData.toString());
+      console.log(data.type);
+      const theTypeOfData = data.type; 
 
-    try {
-      const db = await DBInit();
-      const id = await routeAdd(db, JSON.stringify(data.data)); 
-      console.log("The route id: ", id.data); 
-      console.log("The route data", (await routeGet(db, id.data!)).data); 
+      try {
+        const db = await DBInit();//eventuellt flytta till on connection??
+        switch (theTypeOfData) {
+          case 'Route data':
+            const id = await routeAdd(db, JSON.stringify(data.data)); 
 
+            if(id.data) {
+              //const routeDistance = data.data.routes[0].summary.distance;
+              console.log("The route id: ", id.data); 
+              //groupCreate(db, id.data, );
+            }
 
-      //OBS TAR BORT ALL FINO FRÅN DATABAS 
-      await clearGroups(db); 
-      await clearRoutes(db); 
-      await clearUsers(db); 
+          case 'User data':
+            //lägg in i users 
 
+          case 'Group data':
+            //lägg in i groups 
+
+          case 'id request':
+            //leta upp användare baserat på typ mail 
+
+          case 'Other':
+            console.log(JSON.stringify(data.data));
+
+        }
+        
+  
+  
+        //OBS TAR BORT ALL FINO FRÅN DATABAS 
+        await clearGroups(db); 
+        await clearRoutes(db); 
+        await clearUsers(db); 
+  
+      } catch (err){
+        console.error("Something went wrong :(", err); 
+      }
     } catch (err){
-      console.error("Something went wrong :(", err); 
+      console.error("Could not parse data", err); 
     }
+    
     //parsa data 
     //eventuellt dela upp? / sätt att veta om det är en group??? 
     
--- src/frontend/my-app/components/webSocketsClient.ts
+++ src/frontend/my-app/components/webSocketsClient.ts
@@ -1,38 +1,82 @@
 
 export interface typeOfData{
     type: string, 
-    data: JSON
-}
+    data: JSON, 
+    id: number, 
+    message?: string
+  }
 
 const port = 3000;
-//export const ws = new WebSocket("ws://0.0.0.0:3000"); //LÄGG IN DIN DATORS IP ADDRESS HÄR
+export const ws = new WebSocket("ws://0.0.0.0:3000"); //LÄGG IN DIN DATORS IP ADDRESS HÄR
 
 ws.onopen = () => {
   console.log('[Client] Connected');
   //ws.send('Hi, this is a client');
+  sendToBackend(ws, JSON.parse('{"message": "Hi, this is a client"}'), 5); 
 }
 ws.onmessage = (event) => {
   console.log(`Message from server: ${event.data}`);
   //ws.send("Cool data");
 }
 ws.onerror = (error) => {
-  console.error("WebSocket Error:", error);
+  console.error("WebSocket Error:", error); //borde lägga till något bättre??? 
 }
 
-//1 = Route data, 2 = user data, 3 = group data, 4 = request id
-export function sendToBackend(ws: WebSocket, currentWalkData: JSON, Dataidentifier: number) {
+
+/**
+ * Use this when you want to send data to the backend port 
+ * @param ws webSocket
+ * @param sendData The data you want to send, in JSON format 
+ * @param DataIdentifier 1 = Route data, 2 = user data, 3 = group data, 4 = request id, 5 = other
+ */
+export function sendToBackend(ws: WebSocket, sendData: JSON, DataIdentifier: number) {
   if (ws.readyState === WebSocket.OPEN) {
-    console.log("\n\n\nWalk data: " + JSON.stringify(currentWalkData));
-    //ws.send(Dataidentifier.toString() + JSON.stringify(currentWalkData));
+    console.log("\n\n\nWalk data: " + JSON.stringify(sendData));
+    //ws.send(Dataidentifier.toString() + JSON.stringify(sendData));
     console.log("Message sent to server");
     let result: typeOfData;
-    switch( Dataidentifier ) {
+
+    const userID = 0; //Obs ändra när vi fixat local storage!!!!
+
+    switch( DataIdentifier ) {
         case 1:
             result = {
                 type: 'Route data',
-                data: currentWalkData
+                data: sendData,
+                id: userID
             };
             console.log(JSON.stringify(result));
+            ws.send(JSON.stringify(result));
+        case 2:
+            result = {
+                type: 'User data',
+                data: sendData,
+                id: userID
+            };
+            ws.send(JSON.stringify(result));
+        
+        case 3:
+            result = {
+                type: 'Group data',
+                data: sendData,
+                id: userID
+            };
+            ws.send(JSON.stringify(result));
+
+        case 4: 
+            result = {
+                type: 'id request',
+                data: sendData,
+                id: userID
+            };
+            ws.send(JSON.stringify(result));
+        
+        case 5:
+            result = {
+                type: 'Other',
+                data: sendData,
+                id: userID
+            }
             ws.send(JSON.stringify(result));
     }
 
